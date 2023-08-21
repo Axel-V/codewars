00beb8371a3f9434fe5aaa9aac152951b7c09889
Axel VALLEE <axel.vallee@wefight.co>
2023-08-21 14:29:30 +0200

    chore: Change tree structure and put kata with similar difficulty in the same folder.

---
+++ rust/src/eight_kyu.rs
@@ -0,0 +1,6 @@
+mod are_you_playing_banjo;
+mod basic_mathematical_operations;
+mod fake_binary;
+mod make_uppercase;
+mod remove_string_spaces;
+mod string_repeat;
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,9 +1,4 @@
-mod are_you_playing_banjo;
-mod basic_mathematical_operations;
-mod fake_binary;
-mod make_uppercase;
-mod remove_string_spaces;
-mod string_repeat;
+mod eight_kyu;
 
 fn main() {
     println!("Hello, world!");
